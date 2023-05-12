d4721ed3478dcee0d4d6eea6996c65a68a594638
baizhi958216 <1475289190@qq.com>
2023-05-12 19:20:00 +0800

    feat: 实现注册
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,11 +1,14 @@
 "use client";
 
+import axios from "axios";
 import Button from "@/app/components/Button";
 import Input from "@/app/components/inputs/Input";
 import { useCallback, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import { BsGithub, BsGoogle } from "react-icons/bs";
 import AuthSocialButton from "./AuthSocialButton";
+import { toast } from "react-hot-toast";
+
 type Variant = "LOGIN" | "REGISTER";
 
 const AuthForm = () => {
@@ -36,7 +39,10 @@ const AuthForm = () => {
     setIsLoading(true);
 
     if (variant === "REGISTER") {
-      // Axios Register
+      axios
+        .post("/api/register", data)
+        .catch(() => toast.error("Something went wrong!"))
+        .finally(() => setIsLoading(false));
     }
     if (variant === "LOGIN") {
       // NextAuth SignIn
