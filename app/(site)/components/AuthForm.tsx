ae907aa736d161b48cb5ce002b11a2b6a347e3e5
baizhi958216 <1475289190@qq.com>
2023-05-12 20:03:03 +0800

    feat: 跳转用户页
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -9,19 +9,21 @@ import { BsGithub, BsGoogle } from "react-icons/bs";
 import AuthSocialButton from "./AuthSocialButton";
 import { toast } from "react-hot-toast";
 import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 type Variant = "LOGIN" | "REGISTER";
 
 const AuthForm = () => {
   const session = useSession();
+  const router = useRouter();
   const [variant, setVariant] = useState<Variant>("LOGIN");
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     if (session.status === "authenticated") {
-      console.log("Authenticated");
+      router.push("/users");
     }
-  }, [session.status]);
+  }, [session.status, router]);
 
   const toggleVariant = useCallback(() => {
     if (variant === "LOGIN") {
---
+++ app/users/page.tsx
@@ -0,0 +1,4 @@
+const Users = () => {
+  return <div>Hello Users!</div>;
+};
+export default Users;
