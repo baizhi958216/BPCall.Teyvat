a601eacfa814582148e71d0f8a0711349aae92b2
baizhi958216 <1475289190@qq.com>
2023-05-18 15:13:59 +0800

    fix: auth命名路由
    Signed-off-by: baizhi958216 <1475289190@qq.com>

---
+++ app/BLL/api/auth/route.ts
@@ -0,0 +1,29 @@
+import { pusherServer } from "@/app/UI/libs/pusher";
+import { NextApiRequest, NextApiResponse } from "next";
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+
+export async function handler(request: Request, response: NextApiResponse) {
+  const session = await getServerSession(request as unknown as NextApiRequest);
+  if (!session?.user?.email) {
+    return new NextResponse(null, { status: 403 });
+  }
+
+  const formData = await request.formData();
+  const socketId = formData.get("socket_id");
+  const channelName = formData.get("channel_name");
+
+  const data = {
+    user_id: session.user.email,
+  };
+
+  const authResponse = pusherServer.authorizeChannel(
+    socketId!.toString(),
+    channelName!.toString(),
+    data
+  );
+
+  return new NextResponse(JSON.stringify(authResponse), { status: 200 });
+}
+
+export { handler as POST };
--- app/UI/libs/pusher.ts
+++ app/UI/libs/pusher.ts
@@ -5,7 +5,7 @@ export const pusherClient = new PusherClient(
   process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
   {
     channelAuthorization: {
-      endpoint: "/api/pusher/auth",
+      endpoint: "/BLL/api/auth",
       transport: "ajax",
     },
     cluster: "ap3",
