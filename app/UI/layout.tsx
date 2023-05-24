e41f9ddf6e719ce2bf3ccd7aa3cd0e87f3a6bcb5
baizhi958216 <1475289190@qq.com>
2023-05-24 20:29:46 +0800

    feat: SEO优化
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- app/UI/layout.tsx
+++ app/UI/layout.tsx
@@ -1,6 +1,5 @@
 import AuthContext from "@/app/UI/context/AuthContext";
 import ToasterContext from "@/app/UI/context/ToasterContext";
-
 import "./globals.css";
 import ActiveStatus from "@/app/UI/components/ActiveStatus";
 
@@ -10,7 +9,13 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "提瓦特BB机",
-  description: "旅行者，当你重新踏上旅途之后，一定要记得旅途本身的意义。",
+  description:
+    "提瓦特BB机 -基于Nextjs的即时聊天全栈项目。 旅行者，当你重新踏上旅途之后，一定要记得旅途本身的意义。",
+  keywords:
+    "baizhi958216 原神 ajax实训 react 提瓦特BB机 Nextjs 即时聊天 全栈项目 mongodb atlas prisma",
+  "og:description":
+    "提瓦特BB机 -基于Nextjs的即时聊天全栈项目。 旅行者, 当你重新踏上旅途之后，一定要记得旅途本身的意义",
+  "og:title": "提瓦特BB机",
 };
 
 export default function RootLayout({
